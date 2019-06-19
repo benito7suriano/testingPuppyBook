8052c56b6b46120d03c337982aba387be5c6f036
Beno Suriano <bjsuriano@gmail.com>
2019-06-19 08:00:36 -0600

    Route specs skeleton.

--- server/test/puppy.model.spec.js
+++ server/test/puppy.model.spec.js
@@ -76,4 +76,3 @@ describe('Puppy model', () => {
     })
   })
 })
-
---
+++ server/test/puppy.route.spec.js
@@ -0,0 +1,13 @@
+describe('Puppy routes', () => {
+  describe('GET /api/puppies', () => {
+    it('responds with 200 and all puppies in the db')
+  })
+
+  describe('GET /api/puppies/:id', () => {
+    it('responds with 200 and the correct puppy')
+  })
+
+  describe('POST /api/puppies', () => {
+    it('creates the puppy and responds with 201')
+  })
+})
