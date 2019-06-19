6a0d025fe81e661c1795a96f2b20095430c61380
Beno Suriano <bjsuriano@gmail.com>
2019-06-18 21:15:19 -0600

    Test name validation.

--- server/test/puppy.model.spec.js
+++ server/test/puppy.model.spec.js
@@ -5,8 +5,34 @@ const { expect } = require('chai')
 describe('Puppy model', () => {
   // Schema
   describe('Schema', () => {
+    before('Synchronize the model', () => Puppy.sync({force: true}))
+
+    beforeEach('Truncate data', () => Puppy.truncate())
+
     // it cannot be created without a name
-    it('requires a "name" string')
+    it('requires a "name" string', async () => {
+      try {
+        await Puppy.create()
+        throw new Error('Puppy successfully created with no name')
+      } catch (error) {
+        expect(error.name).to.equal('SequelizeValidationError')
+      }
+
+      try {
+        await Puppy.create('')
+        throw new Error('Puppy sucessfully created with an empty name')
+      } catch (error) {
+        expect(error.name).to.equal('SequelizeValidationError')
+      }
+
+      try {
+        await Puppy.create({name: [1,2,3]})
+        throw new Error('Puppy successfully created with a non-string name')
+      } catch(error) {
+        expect(error.name).to.equal('SequelizeValidationError')
+      }
+    })
+
     // is created with an image type url
     it('expects "image" field to be a URL string if supplied')
     // DOB defaults to `new Date()`
